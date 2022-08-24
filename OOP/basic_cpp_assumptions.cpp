83d9b899c6b376f542359048170024837cd85f10
ramtaruman <sengupta.prakhar@gmail.com>
2022-08-24 15:25:17 +0530

    readme and stuffs

---
+++ OOP/basic_cpp_assumptions.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    // few straightforward examples using the standard data types
+
+    int x = 5;
+    int a = x;
+    float y = 9.87;
+    float y2 = 10.76f; // optional f suffix on float literal
+
+    float b = y;
+
+    double yy = 1243465.534;
+    double c = yy;
+
+    char z = 'Z';
+    char d = 'z';
+
+    bool test = true;
+    bool e = test;
+    bool f = !test;
+
+    char name[10] = "String";
+
+    float grades[20];
+    grades[0] = 4.0;
+}
